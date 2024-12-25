6aefefba165ed96e513c4cd934cccab022445cb9
ronaatashollii <149106227+ronaatashollii@users.noreply.github.com>
2024-12-25 14:35:12 +0100

    Update MainActivity to transition to LoginActivity after 2 second

--- app/src/main/java/com/example/mealbox/MainActivity.java
+++ app/src/main/java/com/example/mealbox/MainActivity.java
@@ -1,4 +1,24 @@
 package com.example.mealbox;
 
-public class MainActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Kaloni në LoginActivity pas 2 sekondash
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // Mbyll MainActivity që të mos mund të kthehesh
+            }
+        }, 2000); // 2000 ms = 2 sekonda
+    }
 }
