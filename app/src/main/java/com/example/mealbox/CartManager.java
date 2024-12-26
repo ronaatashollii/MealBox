66a51961ed55e23cec0ff6ba44833fc30dfa99c7
Venesafejza <154083345+venesafejza66@users.noreply.github.com>
2024-12-26 13:37:28 +0100

    Implemented CartManager with methods to add, remove, and retrieve cart items

---
+++ app/src/main/java/com/example/mealbox/CartManager.java
@@ -0,0 +1,20 @@
+package com.example.mealbox;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartManager {
+    private static final List<CartItem> cartItems = new ArrayList<>();
+
+    public static void addCartItem(CartItem item) {
+        cartItems.add(item);
+    }
+
+    public static void removeCartItem(CartItem item) {
+        cartItems.remove(item);
+    }
+
+    public static List<CartItem> getCartItems() {
+        return new ArrayList<>(cartItems); // Jep një kopje të listës për të shmangur ndryshimet e jashtme
+    }
+}
+
