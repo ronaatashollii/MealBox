555839d1ae055c1f44a0c6c1b0891c440a62fe57
ronaatashollii <149106227+ronaatashollii@users.noreply.github.com>
2024-12-26 23:55:07 +0100

    Updated LoginActivity.java

--- app/src/main/java/com/example/mealbox/LoginActivity.java
+++ app/src/main/java/com/example/mealbox/LoginActivity.java
@@ -60,7 +60,7 @@ public class LoginActivity extends AppCompatActivity {
             Toast.makeText(this, "Enter a valid email", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(this, "Login successful!", Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            startActivity(new Intent(LoginActivity.this, HomePage.class));
             finish();
         }
     }
